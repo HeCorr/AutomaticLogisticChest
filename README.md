1b3d8068d4046de47bc8bb86e5d6a4f8b05df8ea
Belaith <Quanti_T@web.de>
2021-09-05 20:19:52 +0200

    added logic for inserters

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ This mod makes using passive provider chests and requester chests easier.
 .For a visual explanation have alook at
 <https://imgur.com/a/vt1ar>
 
-Upon placement (bot and manual)  of a requester chest all inserters that have that new chest as pickup location have the machines at their droplocations evaluated. For every furnace/machine the material consuption gets calculated, is multiplied by the configured amount and is added to the request of the requester chest. 
+Upon placement of a requester chest all inserters that have that new chest as pickup location have the machines at their droplocations evaluated. For every furnace/machine the material consuption gets calculated, is multiplied by the configured amount and is added to the request of the requester chest. 
 
-Upon placement (bot and manual)  of a passive provider chest all Inserters that have this chest as dropoff location have the machines at their pickup location evaluated. For every furnace/machine the procduction gets calculated. Depending on the configuration the inserter is connected through red or green wire or directly to the logistic network. The inserter is set to only be active when the produced item in the chest or in the network is below the threshhold.
+Upon placement of a passive provider chest all Inserters that have this chest as dropoff location have the machines at their pickup location evaluated. For every furnace/machine the procduction gets calculated. Depending on the configuration the inserter is connected through red or green wire or directly to the logistic network. The inserter is set to only be active when the produced item in the chest or in the network is below the threshhold.
 The threshhold is calculated through the production of the machine.
+
+Upon Placement of a inserter get entitys on pickup and drop location and if it's a logistic chest goes through the same logic.
 
 How is it calculated?
 The consumption and production takes moduls and beacons into consideration.
--- control.lua
+++ control.lua
@@ -28,135 +28,153 @@ script.on_event(defines.events.on_robot_built_entity, function(event)
 	end
 end)
 
-function handleEvent(chest)
-	if chest ~= nil and chest.type ~= nil and chest.type == "logistic-container" then
-		local bufferTimeRequester = settings.global["AutomaticLogisticChest-BuffertimeRequester"].value
-		local bufferTimeProvider = settings.global["AutomaticLogisticChest-BuffertimeProvider"].value
+function handleEvent(entity)
+	if entity ~= nil and entity.type ~= nil then
+		if entity.type == "inserter" then
+			
+			local pickup_target = getPickupTarget(entity)
+			local drop_target = getDropTarget(entity)
 
-		if (bufferTimeRequester == 0 and bufferTimeProvider == 0) then
-			return
-		end
+			if pickup_target ~= nil and pickup_target.type ~= nil and pickup_target.type == "logistic-container" then
+				handleEvent(pickup_target)
+			end
+			if drop_target ~= nil and drop_target.type ~= nil and drop_target.type == "logistic-container" then
+				handleEvent(drop_target)
+			end
 
-		local connectionType = settings.global["AutomaticLogisticChest-ConnectionType"].value
-		local overrideExisting = settings.global["AutomaticLogisticChest-OverrideExisting"].value
+		elseif entity.type == "logistic-container" then
+			local bufferTimeRequester = settings.global["AutomaticLogisticChest-BuffertimeRequester"].value
+			local bufferTimeProvider = settings.global["AutomaticLogisticChest-BuffertimeProvider"].value
+
+			if (bufferTimeRequester == 0 and bufferTimeProvider == 0) then
+				return
+			end
+
+			local connectionType = settings.global["AutomaticLogisticChest-ConnectionType"].value
+			local overrideExisting = settings.global["AutomaticLogisticChest-OverrideExisting"].value
+			
+			local minRequester = settings.global["AutomaticLogisticChest-MinRequester"].value
+			local maxRequester = settings.global["AutomaticLogisticChest-MaxRequester"].value
+			local minProvider = settings.global["AutomaticLogisticChest-MinProvider"].value
+			local maxProvider = settings.global["AutomaticLogisticChest-MaxProvider"].value
 		
-		local minRequester = settings.global["AutomaticLogisticChest-MinRequester"].value
-		local maxRequester = settings.global["AutomaticLogisticChest-MaxRequester"].value
-		local minProvider = settings.global["AutomaticLogisticChest-MinProvider"].value
-		local maxProvider = settings.global["AutomaticLogisticChest-MaxProvider"].value
-	
-		if chest.prototype ~= nil and chest.prototype.logistic_mode ~= nil and (chest.prototype.logistic_mode == "requester" or chest.prototype.logistic_mode == "passive-provider") then
-			local inserters = chest.surface.find_entities_filtered(
-			{
-				area =
+			if entity.prototype ~= nil and entity.prototype.logistic_mode ~= nil and (entity.prototype.logistic_mode == "requester" or entity.prototype.logistic_mode == "passive-provider") then
+				local inserters = entity.surface.find_entities_filtered(
 				{
+					area =
 					{
-						x = chest.position.x - 2,
-						y = chest.position.y - 2
+						{
+							x = entity.position.x - 3,
+							y = entity.position.y - 3
+						},
+						{
+							x = entity.position.x + 3,
+							y = entity.position.y + 3
+						}
 					},
-					{
-						x = chest.position.x + 2,
-						y = chest.position.y + 2
-					}
-				},
-				type = "inserter"
-			})
-		
-			if #inserters > 0 then
-				
-				if (chest.prototype.logistic_mode == "requester" and bufferTimeRequester > 0) then
-					local inputs = {}
+					type = "inserter"
+				})
+			
+				if (#inserters > 0) then
 					
-					for inserter = 1, #inserters do
-						if inserters[inserter].pickup_target == chest or (inserters[inserter].pickup_target == nil and comparePositions(inserters[inserter].pickup_position, chest.position)) then
-							if inserters[inserter].drop_target ~= nil and (inserters[inserter].drop_target.type == "assembling-machine" or inserters[inserter].drop_target.type == "furnace") and inserters[inserter].drop_target.get_recipe() ~= nil then
-								calcInputs(inserters[inserter].drop_target, inputs, bufferTimeRequester)
+					if (entity.prototype.logistic_mode == "requester" and bufferTimeRequester > 0) then
+						local inputs = {}
+						
+						for inserter = 1, #inserters do
+							local pickupTarget = getPickupTarget(inserters[inserter])
+							if (pickupTarget ~= nil and pickupTarget == entity) then
+								local dropTarget = getDropTarget(inserters[inserter])
+								if (dropTarget ~= nil and dropTarget.type ~= nil and (dropTarget.type == "assembling-machine" or dropTarget.type == "furnace") and dropTarget.get_recipe() ~= nil) then
+									calcInputs(dropTarget, inputs, bufferTimeRequester)
+								end
 							end
 						end
-					end
 
-					if next(inputs) ~= nil then					
-						for requestSlot = 1, chest.request_slot_count do
-							chest.clear_request_slot(requestSlot)
-						end
-
-						local slot = 1
-						for itemName in pairs(inputs) do
-							local itemCount = math.ceil(inputs[itemName])
-
-							local proto = game.item_prototypes[itemName]
-							if (minRequester > 0 and itemCount < proto.stack_size * minRequester) then
-								itemCount = proto.stack_size * minRequester
+						if (next(inputs) ~= nil) then					
+							for requestSlot = 1, entity.request_slot_count do
+								entity.clear_request_slot(requestSlot)
 							end
 
-							if (maxRequester > 0 and itemCount > proto.stack_size * maxRequester) then
-								itemCount = proto.stack_size * maxRequester
+							local slot = 1
+							for itemName in pairs(inputs) do
+								local itemCount = math.ceil(inputs[itemName])
+
+								local proto = game.item_prototypes[itemName]
+								if (minRequester > 0 and itemCount < proto.stack_size * minRequester) then
+									itemCount = proto.stack_size * minRequester
+								end
+
+								if (maxRequester > 0 and itemCount > proto.stack_size * maxRequester) then
+									itemCount = proto.stack_size * maxRequester
+								end
+
+								entity.set_request_slot(
+								{
+									name = itemName,
+									count = math.ceil(itemCount)
+								}, slot)
+								slot = slot + 1
 							end
-
-							chest.set_request_slot(
-							{
-								name = itemName,
-								count = math.ceil(itemCount)
-							}, slot)
-							slot = slot + 1
 						end
-					end
-				elseif (chest.prototype.logistic_mode == "passive-provider" and bufferTimeProvider > 0) then
-					for inserter = 1, #inserters do
-						if inserters[inserter].drop_target == chest or (inserters[inserter].drop_target == nil and comparePositions(inserters[inserter].drop_position, chest.position)) then
-							if inserters[inserter].pickup_target ~= nil and (inserters[inserter].pickup_target.type == "assembling-machine" or inserters[inserter].pickup_target.type == "furnace") and inserters[inserter].pickup_target.get_recipe() ~= nil then
-								
-								local outputs = {}
-								calcOutputs(inserters[inserter].pickup_target, outputs, bufferTimeProvider)	
-								
-								for itemName in pairs(outputs) do
-									local itemCount = math.ceil(outputs[itemName])
-
-									local proto = game.item_prototypes[itemName]
-									if (minProvider > 0 and itemCount < proto.stack_size * minProvider) then
-										itemCount = proto.stack_size * minProvider
-									end
-
-									if (maxProvider > 0 and itemCount > proto.stack_size * maxProvider) then
-										itemCount = proto.stack_size * maxProvider
-									end
-
-									local condition = 
-									{
-										condition = 
-										{
-											comparator = "<",
-											first_signal =
-											{
-												type = "item",
-												name = itemName
-											},
-											constant = math.ceil(itemCount)
-										}
-									}
+					elseif (entity.prototype.logistic_mode == "passive-provider" and bufferTimeProvider > 0) then
+						for inserter = 1, #inserters do
+							local dropTarget = getDropTarget(inserters[inserter])
+							if (dropTarget ~= nil and dropTarget == entity) then
+								local pickupTarget = getPickupTarget(inserters[inserter])
+								if (pickupTarget ~= nil and pickupTarget.type ~= nil and (pickupTarget.type == "assembling-machine" or pickupTarget.type == "furnace") and pickupTarget.get_recipe() ~= nil) then
 									
-									local controlBehavior = inserters[inserter].get_or_create_control_behavior()
+									local outputs = {}
+									calcOutputs(pickupTarget, outputs, bufferTimeProvider)	
+									
+									for itemName in pairs(outputs) do
+										local itemCount = math.ceil(outputs[itemName])
 
-									if overrideExisting or not (controlBehavior.get_circuit_network(defines.wire_type.green) ~=nil or controlBehavior.get_circuit_network(defines.wire_type.red) ~=nil or controlBehavior.connect_to_logistic_network == true) then
-										if connectionType == "Logistic"  then
-											controlBehavior.connect_to_logistic_network = true
-											controlBehavior.logistic_condition = condition
-										else
-											if connectionType == "GreenCable" then
-												chest.connect_neighbour(
+										local proto = game.item_prototypes[itemName]
+										if (minProvider > 0 and itemCount < proto.stack_size * minProvider) then
+											itemCount = proto.stack_size * minProvider
+										end
+
+										if (maxProvider > 0 and itemCount > proto.stack_size * maxProvider) then
+											itemCount = proto.stack_size * maxProvider
+										end
+
+										local condition = 
+										{
+											condition = 
+											{
+												comparator = "<",
+												first_signal =
 												{
-													wire = defines.wire_type.green,
-													target_entity = inserters[inserter]
-												})
+													type = "item",
+													name = itemName
+												},
+												constant = math.ceil(itemCount)
+											}
+										}
+										
+										local controlBehavior = inserters[inserter].get_or_create_control_behavior()
+
+										if (overrideExisting or not (controlBehavior.get_circuit_network(defines.wire_type.green) ~=nil or controlBehavior.get_circuit_network(defines.wire_type.red) ~=nil or controlBehavior.connect_to_logistic_network == true)) then
+											if (connectionType == "Logistic")  then
+												controlBehavior.connect_to_logistic_network = true
+												controlBehavior.logistic_condition = condition
 											else
-												chest.connect_neighbour(
-												{
-													wire = defines.wire_type.red,
-													target_entity = inserters[inserter]
-												})
-											end
+												if (connectionType == "GreenCable") then
+													entity.connect_neighbour(
+													{
+														wire = defines.wire_type.green,
+														target_entity = inserters[inserter]
+													})
+												else
+													entity.connect_neighbour(
+													{
+														wire = defines.wire_type.red,
+														target_entity = inserters[inserter]
+													})
+												end
 
-											controlBehavior.circuit_condition = condition
+												controlBehavior.circuit_condition = condition
+											end
 										end
 									end
 								end
@@ -169,9 +187,36 @@ function handleEvent(chest)
 	end
 end
 
-function comparePositions(position1, position2)
-	
-	return position1.x > position2.x - 0.5 and position1.x < position2.x + 0.5 and position1.y > position2.y - 0.5 and position1.y < position2.y + 0.5
+function getPickupTarget(inserter)
+	if (inserter.pickup_target ~= nil) then
+		return inserter.pickup_target
+	else
+		local pickup_targets = inserter.surface.find_entities_filtered(
+			{
+				position = inserter.pickup_position
+			})
+		if (#pickup_targets > 0) then
+			return pickup_targets[1]
+		else
+			return nil
+		end
+	end
+end
+
+function getDropTarget(inserter)
+	if (inserter.drop_target ~= nil) then
+		return inserter.drop_target
+	else
+		local drop_targets = inserter.surface.find_entities_filtered(
+			{
+				position = inserter.drop_position
+			})
+		if (#drop_targets > 0) then
+			return drop_targets[1]
+		else
+			return nil
+		end
+	end
 end
 
 -- calculate the required input for an entity
@@ -183,18 +228,18 @@ function calcInputs(entity, inputs, bufferTime)
 	-- if craftingtime > bufferTime buffer enough items for one craft, else buffer how much the entity consumes in the bufferTime
 	for _, ingred in ipairs(entity.get_recipe().ingredients) do
 		local amount = 0
-		if ingred.type == "item" then
-			if ingred.amount ~= nil then
+		if (ingred.type == "item") then
+			if (ingred.amount ~= nil) then
 				amount = ingred.amount
 			end
 		end
 		
-		if amount > 0 then
+		if (amount > 0) then
 			if(craftingTime < bufferTime) then
 				amount = (amount / craftingTime) * bufferTime
 			end
 			
-			if inputs[ingred.name] == nil then
+			if (inputs[ingred.name] == nil) then
 				inputs[ingred.name] = 0
 			end
 					
@@ -212,25 +257,25 @@ function calcOutputs(entity, outputs, bufferTime)
 	-- if craftingtime > bufferTime buffer the items of one craft, else buffer how much the entity crafts in the bufferTime
 	for _, product in ipairs(entity.get_recipe().products) do
 		local amount = 0
-		if product.type == "item" then
-			if product.amount ~= nil then
+		if (product.type == "item")then
+			if (product.amount ~= nil) then
 				amount = product.amount
-			elseif product.amount_min ~= nil and product.amount_max ~= nil and product.probability ~= nil then
+			elseif (product.amount_min ~= nil and product.amount_max ~= nil and product.probability ~= nil) then
 				amount = ((product.amount_min + product.amount_max)/ 2) * product.probability
 			end
 		end
 		
-		if amount > 0 then
+		if (amount > 0) then
 			amount = amount * (1 + entity.productivity_bonus)
 
 			if(craftingTime < bufferTime) then
 				amount = (amount / craftingTime) * bufferTime
 			end
 			
-			if outputs[product.name] == nil then
+			if (outputs[product.name] == nil) then
 				outputs[product.name] = 0
 			end
-					
+			
 			outputs[product.name] = outputs[product.name] + amount
 		end
 	end
